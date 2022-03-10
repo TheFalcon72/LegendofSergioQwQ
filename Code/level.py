45ae41cbca46817fe4f292b101379c68cb00e844
Sergio A Samano <sergio.checo09.ss@gmail.com>
2022-03-09 18:22:53 -0600

    09/03/2022 6:22 PM

--- Code/level.py
+++ Code/level.py
@@ -4,13 +4,12 @@ from tile import Tile
 from player import Player
 from debug import debug
 
+
 class Level:
     def __init__(self):
-        # Get the display surface
-        self.display_surface = pygame.display.get_surface()
 
         # Sprite Group Setup
-        self.visible_sprite = pygame.sprite.Group()
+        self.visible_sprite = YSortCamaraGroup()
         self.obstacles_sprite = pygame.sprite.Group()
 
         # Sprite Setup
@@ -24,10 +23,30 @@ class Level:
                 if col == 'x':
                     Tile((x, y), [self.visible_sprite, self.obstacles_sprite])
                 if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprite])
+                    self.player = Player((x, y), [self.visible_sprite], self.obstacles_sprite)
 
     def run(self):
         # Update and Draw the game
-        self.visible_sprite.draw(self.display_surface)
+        self.visible_sprite.custom_draw(self.player)
         self.visible_sprite.update()
-        debug(self.player.direction)
+
+
+class YSortCamaraGroup(pygame.sprite.Group):
+    def __init__(self):
+        # general setup
+        super().__init__()
+
+        # Get the display surface
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft + self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
--- Code/player.py
+++ Code/player.py
@@ -2,12 +2,15 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
 
         self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+        self.obstacle_sprites = obstacle_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -28,5 +31,33 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.rect.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.collision('vertical')
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:
+                        self.rect.bottom = sprite.rect.top
+
+                    if self.direction.y < 0:
+                        self.rect.top = sprite.rect.bottom
+
     def update(self):
         self.input()
+        self.move(self.speed)
